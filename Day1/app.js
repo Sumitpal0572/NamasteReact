3bbbad03dd7a443fac857023fda70045103a4fb4
Sumitpal0572 <asmyselfsumit@gmail.com>
2025-05-14 00:22:21 +0530

    Nested Element created using react

---
+++ Day1/app.js
@@ -0,0 +1,16 @@
+
+// create the nested element using React 
+
+// <div id="parent">
+//     <div id="child">
+//         <h1>Hey I"m H1</h1>
+//     </div>
+// </div>
+
+const parent = React.createElement("div", { id: "parent" },
+    React.createElement("div", { id: "child" }),
+    React.createElement("h1", {}, "Hey I'm H1")
+)
+
+const root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(parent);
