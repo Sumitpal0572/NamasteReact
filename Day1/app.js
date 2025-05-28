24934ef2798c52d41ed93ed0eb0d6725d2cabc23
Sumitpal0572 <asmyselfsumit@gmail.com>
2025-05-28 16:54:10 +0530

    Create more then one element inside

--- Day1/app.js
+++ Day1/app.js
@@ -7,9 +7,13 @@
 //     </div>
 // </div>
 
-const parent = React.createElement("div", { id: "parent" },
+const parent = React.createElement("div", { id: "parent" }, [
     React.createElement("div", { id: "child" }),
-    React.createElement("h1", {}, "Hey I'm H1")
+    React.createElement("h1", {}, "Hey I'm H1"),
+    React.createElement("h2", {}, "Hey I'm H2")
+
+]
+
 )
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
