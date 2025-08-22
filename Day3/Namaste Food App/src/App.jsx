9efc1e932f573d03dcf5404e65bb089ec42e95dd
Sumitpal0572 <asmyselfsumit@gmail.com>
2025-08-22 23:41:10 +0530

    Food app setup

---
+++ Day3/Namaste Food App/src/App.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import "./App.css";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo-items">
+        <img
+          className="logo"
+          src="https://d1csarkz8obe9u.cloudfront.net/posterpreviews/cook-logo-food-logo-design-template-853a1304372e26953ffd93f44d7b6cf5_screen.jpg?ts=1732296111"
+          alt=""
+        />
+      </div>
+
+      <div className="nav-links">
+        <ul>
+          <li>Home </li>
+          <li>About</li>
+          <li>Cart </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+const AppLayout = () => {
+  return (
+    <div className="header">
+      <Header />
+    </div>
+  );
+};
+
+export default AppLayout;
